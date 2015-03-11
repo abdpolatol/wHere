24d45c337fdd065f86702c9c8a73c01d93507b23
alisihab <sihab.akcan@gmail.com>
2015-03-11 16:16:58 +0200

    11.3.2015 by ali 16:16

--- src/com/example/gspot/slidingmenu/MyProfileFragment.java
+++ src/com/example/gspot/slidingmenu/MyProfileFragment.java
@@ -131,6 +131,12 @@ public class MyProfileFragment extends Fragment implements PullScrollView.OnTurn
         		editProfile.setText("Add Friend");
         		//editProfile.setCompoundDrawablesWithIntrinsicBounds(R.drawable.waiting, 0, 0, 0);
         	}
+        	if(flag==4){
+        		username.setText(friendName+" "+friendSurname);
+        		new LoadImage().execute(friendPhoto);
+        		editProfile.setText("Accept Request");
+        		editProfile.setCompoundDrawablesWithIntrinsicBounds(R.drawable.confirm, 0, 0, 0);
+        	}
     	}      
     }
 	protected void initView() {
