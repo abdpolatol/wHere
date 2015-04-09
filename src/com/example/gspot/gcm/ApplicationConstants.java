f715989dc058d06630ac00a327d421dba268c60c
alisihab <sihab.akcan@gmail.com>
2015-04-09 16:49:40 +0300

    9.4.2015 16:50 ali

---
+++ src/com/example/gspot/gcm/ApplicationConstants.java
@@ -0,0 +1,12 @@
+package com.example.gspot.gcm;
+
+public interface ApplicationConstants {
+
+	// Php Application URL to store Reg ID created
+	static final String APP_SERVER_URL = "http://www.ceng.metu.edu.tr/~e1818871/insertuser.php";
+
+	// Google Project Number
+	static final String GOOGLE_PROJ_ID = "833708658859";
+	static final String MSG_KEY = "m";
+
+}
